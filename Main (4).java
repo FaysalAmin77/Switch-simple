f27b341d0c9906bfeb656121f290aa6e5b545b15
FaysalAmin77 <95224798+FaysalAmin77@users.noreply.github.com>
2021-12-24 03:09:54 -0800

    Add files via upload

---
+++ Main (4).java
@@ -0,0 +1,22 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+	
+
+	switch(276){
+	    
+	    case 23 :
+	        System.out.println("Number is 23");
+	        break;
+	        case 65:
+	            System.out.println("Number is 65");
+	            break;
+	            default:
+	            System.out.println("Wrong number ");
+	        
+	    
+	     }
+	}
+	
+}
